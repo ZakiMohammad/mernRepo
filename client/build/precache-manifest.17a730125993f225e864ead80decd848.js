2310cd4f1e12e176b1fc175117559b9fdf646b03
zaki <zeusreus011@gmail.com>
2020-03-21 22:19:33 +0530

    so many errors

---
+++ client/build/precache-manifest.17a730125993f225e864ead80decd848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a92b5443d9b13f3f0280155c90140f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "608768a6fcc2591fe0c1",
+    "url": "/static/css/main.af433a4b.chunk.css"
+  },
+  {
+    "revision": "6d523c85fac35c3dd81b",
+    "url": "/static/js/2.b01d88c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b01d88c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608768a6fcc2591fe0c1",
+    "url": "/static/js/main.556a4da0.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
